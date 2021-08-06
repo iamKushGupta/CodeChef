f31fcfaba2b25a3827a8cec895979d5ad6fc129d
Kush Gupta <74309241+iamKushGupta@users.noreply.github.com>
2021-08-06 17:53:10 +0530

    Create CHFHEIST.c

---
+++ CHFHEIST.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+
+int main () {
+    int t, a, b, x, y;
+    scanf("%d", &t);
+    for(int i= 0; i<t; i++ ){
+        scanf ("%d %d %d %d", &a, &b, &x, &y);
+        printf("%d\n", (x/a) + (y/b));
+    }
+    return 0;
+}
