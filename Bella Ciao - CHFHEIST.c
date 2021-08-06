6ad33bc0e92d60c13df217a7bc891b6888f43bef
Kush Gupta <74309241+iamKushGupta@users.noreply.github.com>
2021-08-06 17:55:43 +0530

    Update and rename CHFHEIST.c to Bella Ciao - CHFHEIST.c

--- Bella Ciao - CHFHEIST.c
+++ Bella Ciao - CHFHEIST.c
@@ -4,9 +4,11 @@
 int main () {
     int t, a, b, x, y;
     scanf("%d", &t);
-    for(int i= 0; i<t; i++ ){
+    
+    for(int i= 0; i<t; i++ ) {
         scanf ("%d %d %d %d", &a, &b, &x, &y);
         printf("%d\n", (x/a) + (y/b));
     }
+    
     return 0;
 }
